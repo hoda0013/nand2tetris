221761d16f41e4a5f53349046744b8698d2cd0a0
Dillon Hodapp <hoda0013@gmail.com>
2017-10-17 22:57:00 -0500

    added character and scanner classes

--- Nand2TetrisPlayground/nand2tetris/src/main/java/com/example/MyMain.java
+++ Nand2TetrisPlayground/nand2tetris/src/main/java/com/example/MyMain.java
@@ -1,17 +1,21 @@
 package com.example;
 
-import java.io.IOException;
-
 public class MyMain {
     public static void main(String args[]) {
-        MyParser myParser = new MyParser("/Users/bigern/Projects/nand2tetris/projects/06/max/Max.asm");
-        while (myParser.hasMoreCommands()) {
-            try {
-                myParser.advance();
-            } catch (IOException e) {
-                e.printStackTrace();
-                break;
-            }
+//        MyParser myParser = new MyParser("/Users/bigern/Projects/nand2tetris/projects/06/max/Max.asm");
+//        while (myParser.hasMoreCommands()) {
+//            try {
+//                myParser.advance();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                break;
+//            }
+//        }
+
+        MyScanner myScanner = new MyScanner("/Users/bigern/Projects/nand2tetris/projects/06/max/Max.asm");
+        MyCharacter character;
+        while((character = myScanner.get()) != null)
+            System.out.println(character.toString());
         }
-    }
+
 }
