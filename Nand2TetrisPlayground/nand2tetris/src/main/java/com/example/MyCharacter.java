1d009f7c09a1025781728f87b1b6d276f2e8c5c5
Dillon Hodapp <hoda0013@gmail.com>
2017-10-17 22:57:15 -0500

    added scanner and character classes

---
+++ Nand2TetrisPlayground/nand2tetris/src/main/java/com/example/MyCharacter.java
@@ -0,0 +1,38 @@
+package com.example;
+
+/**
+ * Created by DJH on 10/17/17.
+ */
+
+public class MyCharacter {
+    private char mCargo;
+    private long mSourceIndex;
+    private long mLineIndex;
+    private long mColumnIndex;
+
+    public MyCharacter(char cargo, long sourceIndex, long lineIndex, long columnIndex) {
+        mCargo = cargo;
+        mSourceIndex = sourceIndex;
+        mLineIndex = lineIndex;
+        mColumnIndex = columnIndex;
+    }
+
+    @Override
+    public String toString() {
+        String value = String.valueOf(mSourceIndex)
+                + " "
+                + String.valueOf(mLineIndex)
+                + " "
+                + String.valueOf(mColumnIndex)
+                + " ";
+        if(mCargo == ' ') {
+            return value + "space";
+        } else if (mCargo == '\n') {
+            return value + "newline";
+        } else if (mCargo == '\t') {
+            return value + "tab";
+        } else {
+            return value + String.valueOf(mCargo);
+        }
+    }
+}
