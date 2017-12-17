9f22b677e9b831657d385a0e97bae46cffdf762f
Dillon Hodapp <hoda0013@gmail.com>
2017-12-16 23:11:46 -0600

    got stacktext working

--- Nand2TetrisPlayground/nand2tetris/src/main/java/com/example/chapter7/CodeWriter.java
+++ Nand2TetrisPlayground/nand2tetris/src/main/java/com/example/chapter7/CodeWriter.java
@@ -15,6 +15,9 @@ public class CodeWriter {
     private BufferedWriter mBufferedWriter;
 
     private String mInputFilename;
+    private int mNumEqCalls = 0;
+    private int mNumGtCalls = 0;
+    private int mNumLtCalls = 0;
 
     public CodeWriter(String outputFilename) {
         mOutputFilename = outputFilename;
@@ -39,46 +42,77 @@ public class CodeWriter {
         try {
             switch (command) {
                 case "add":
-                    //get SP, decrement and store in R0
+                    //Decrement SP and store in R0
                     writeCommandAndNewline("@R0"); //A=0
                     writeCommandAndNewline("M=M-1"); //M = SP - 1
 
-                    //get value of M[SP]
-                    writeCommandAndNewline("A=M"); //A = SP
+                    //Get value pointed at by SP and store in D register
+                    writeCommandAndNewline("A=M"); //A = M[0] = SP
                     writeCommandAndNewline("D=M"); //D = M[SP]
 
-                    //decrement SP and add value it points to to D
-                    writeCommandAndNewline("A=A-1"); //Decrement SP
-                    writeCommandAndNewline("D=D+M"); //D = M[SP] + M[SP - 1}
-                    writeCommandAndNewline("M=D"); //M[SP] = D
+                    //decrement SP
+                    writeCommandAndNewline("@R0"); //A=0
+                    writeCommandAndNewline("M=M-1"); //M = SP - 1
+
+                    //Add value pointed at by SP to value in D register and store in D register
+                    writeCommandAndNewline("A=M"); //A = M[0] = SP
+                    writeCommandAndNewline("D=D+M");
+
+                    //Save added value in location pointed to by SP
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("A=M"); //A = M[0] = SP
+                    writeCommandAndNewline("M=D"); //M[SP] = D = sum
+
+                    //increment SP
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("M=M+1");
                     break;
                 case "sub":
                     //get SP, decrement and store in R0
                     writeCommandAndNewline("@R0"); //A=0
-                    writeCommandAndNewline("M=M-1"); //M = SP - 1
+                    writeCommandAndNewline("M=M-1"); //M[0] = M[0] - 1
 
                     //get value of M[SP]
-                    writeCommandAndNewline("A=M"); //A = SP
+                    writeCommandAndNewline("A=M"); //A = M[0] = SP
                     writeCommandAndNewline("D=M"); //D = M[SP]
 
-                    //decrement SP and subtract value it points to from D
-                    writeCommandAndNewline("A=A-1"); //Decrement SP
-                    writeCommandAndNewline("D=M-D"); //D = M[SP] - M[SP - 1}
-                    writeCommandAndNewline("M=D"); //M[SP] = D
+                    //decrement SP
+                    writeCommandAndNewline("@R0"); //A=0
+                    writeCommandAndNewline("M=M-1"); //M[0] = M[0] - 1
+
+                    //Subtract y from x, that is, value pointed at by SP - value in D register
+                    writeCommandAndNewline("A=M"); //A = M[0] = SP
+                    writeCommandAndNewline("D=M-D"); //D = M[A] - D = M[SP] - D
+
+                    //Save added value in location pointed to by SP
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("A=M"); //A = M[0] = SP
+                    writeCommandAndNewline("M=D"); //M[SP] = D = sub value
+
+                    //increment SP
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("M=M+1");
                     break;
                 case "neg":
+                    //TODO: update this method to be like add and sub
                     //2's complement way of making a number have the opposite sign
                     //To make a positive value negative, write out the positive number in binary, invert the digits, add one
 
-                    //get SP, decrement and store in R0
+                    //Decrement SP
                     writeCommandAndNewline("@R0"); //A=0
                     writeCommandAndNewline("M=M-1"); //M = SP - 1
 
                     //get value of M[SP], store in D reg
                     writeCommandAndNewline("A=M"); //A = SP
-                    writeCommandAndNewline("D=M"); //D = M[SP]
+                    writeCommandAndNewline("D=-M"); //D = M[SP]
 
-                    writeCommandAndNewline("D=-D");
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("A=M");
+                    writeCommandAndNewline("M=D");
+
+                    //increment SP
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("M=M+1");
                     break;
 
                 case "eq":
@@ -91,25 +125,35 @@ public class CodeWriter {
                     writeCommandAndNewline("D=M"); //D = M[SP]
 
                     //compute x-y, then if equals zero, jump
-                    writeCommandAndNewline("A=A-1"); //Decrement SP
+                    writeCommandAndNewline("@R0"); //A=0
+                    writeCommandAndNewline("M=M-1");
+                    writeCommandAndNewline("A=M");
                     writeCommandAndNewline("D=M-D"); //D = M[SP] - M[SP - 1}
-                    writeCommandAndNewline("@ZERO");
+                    writeCommandAndNewline("@EQ" + "_" + String.valueOf(mNumEqCalls));
                     writeCommandAndNewline("D;JEQ");
+
                     writeCommandAndNewline("@R0"); //not equal
                     writeCommandAndNewline("A=M");
                     writeCommandAndNewline("M=0"); //x-y != 0, false
-                    writeCommandAndNewline("@DONE");
+                    writeCommandAndNewline("@EQ_DONE" + "_" + String.valueOf(mNumEqCalls));
                     writeCommandAndNewline("0;JMP");
 
-                    writeCommandAndNewline("(ZERO)");
+                    writeCommandAndNewline("(EQ_" + String.valueOf(mNumEqCalls) + ")");
                     writeCommandAndNewline("@R0"); //A=0
                     writeCommandAndNewline("A=M"); //A=M[0] = SP value
                     writeCommandAndNewline("M=-1"); //x-y == 0, M[SP] = true
-                    writeCommandAndNewline("(DONE)");
+
+                    writeCommandAndNewline("(EQ_DONE_" +     String.valueOf(mNumEqCalls) + ")");
+
+                    //increment SP
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("M=M+1");
+                    mNumEqCalls++;
                     break;
 
 
                 case "gt":
+                    //TODO: fix so it works like the equals function, Increment SP at the end, Set SP value to R0 when you increment it
                     //get SP, decrement and store in R0
                     writeCommandAndNewline("@R0"); //A=0
                     writeCommandAndNewline("M=M-1"); //M = SP - 1
@@ -118,17 +162,35 @@ public class CodeWriter {
                     writeCommandAndNewline("A=M"); //A = SP
                     writeCommandAndNewline("D=M"); //D = M[SP]
 
+                    //get SP, decrement and store in R0
+                    writeCommandAndNewline("@R0"); //A=0
+                    writeCommandAndNewline("M=M-1"); //M = SP - 1
+
                     //compute x-y, if positive x is greater than y
-                    writeCommandAndNewline("A=A-1"); //Decrement SP
-                    writeCommandAndNewline("D=M-D"); //D = M[SP] - M[SP - 1}
-                    writeCommandAndNewline("@POSITIVE");
+                    writeCommandAndNewline("A=M");
+                    writeCommandAndNewline("D=M-D"); //D = M[SP] - M[SP - 1], if D > 0, then x > y
+                    writeCommandAndNewline("@GT_" + String.valueOf(mNumGtCalls));
                     writeCommandAndNewline("D;JGT");
-                    writeCommandAndNewline("@DONE");
+
+                    //x <= y
+                    writeCommandAndNewline("@R0"); //not equal
+                    writeCommandAndNewline("A=M");
+                    writeCommandAndNewline("M=0"); //false
+                    writeCommandAndNewline("@GT_DONE_"+ String.valueOf(mNumGtCalls));
                     writeCommandAndNewline("0;JMP");
-                    writeCommandAndNewline("M=0"); //x-y <= 0, false
-                    writeCommandAndNewline("(POSITIVE)");
+
+                    //x > y
+                    writeCommandAndNewline("(GT_"+ String.valueOf(mNumGtCalls) + ")");
+                    writeCommandAndNewline("@R0"); //not equal
+                    writeCommandAndNewline("A=M");
                     writeCommandAndNewline("M=-1"); //x-y > 0, true
-                    writeCommandAndNewline("(DONE)");
+                    writeCommandAndNewline("(GT_DONE_" + String.valueOf(mNumGtCalls) + ")");
+
+                    //increment SP
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("M=M+1");
+                    mNumGtCalls++;
+                    break;
 
                 case "lt":
                     //get SP, decrement and store in R0
@@ -136,21 +198,36 @@ public class CodeWriter {
                     writeCommandAndNewline("M=M-1"); //M = SP - 1
 
                     //get value of M[SP], store in D reg
-                    writeCommandAndNewline("A=M"); //A = SP
-                    writeCommandAndNewline("D=M"); //D = M[SP]
+                    writeCommandAndNewline("A=M"); //A = SP = M[0]
+                    writeCommandAndNewline("D=M"); //D = M[SP] = y
+
+                    //get SP, decrement and store in R0
+                    writeCommandAndNewline("@R0"); //A=0
+                    writeCommandAndNewline("M=M-1"); //M = SP - 1
 
                     //compute x-y, if negative x is greater than y
-                    writeCommandAndNewline("A=A-1"); //Decrement SP
-                    writeCommandAndNewline("D=M-D"); //D = M[SP] - M[SP - 1}
-                    writeCommandAndNewline("@NEGATIVE");
+                    writeCommandAndNewline("A=M"); //A = M[0] = SP
+                    writeCommandAndNewline("D=M-D"); //D = M[SP] - M[SP - 1] = x - y
+                    writeCommandAndNewline("@LT_" + String.valueOf(mNumLtCalls));
                     writeCommandAndNewline("D;JLT");
-                    writeCommandAndNewline("@DONE");
+                    //x >= y
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("A=M"); //A = SP
+                    writeCommandAndNewline("M=0"); //false M[SP] = 0
+                    writeCommandAndNewline("@LT_DONE_" + String.valueOf(mNumLtCalls));
                     writeCommandAndNewline("0;JMP");
-                    writeCommandAndNewline("M=0"); //x-y >= 0, false
-                    writeCommandAndNewline("(NEGATIVE)");
-                    writeCommandAndNewline("M=-1"); //x-y < 0, true
-                    writeCommandAndNewline("(DONE)");
 
+                    //x < y
+                    writeCommandAndNewline("(LT_" + String.valueOf(mNumLtCalls) + ")");
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("A=M");
+                    writeCommandAndNewline("M=-1"); //true
+                    writeCommandAndNewline("(LT_DONE_" + String.valueOf(mNumLtCalls) + ")");
+
+                    //increment SP
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("M=M+1");
+                    mNumLtCalls++;
                     break;
 
                 case "and":
@@ -162,9 +239,18 @@ public class CodeWriter {
                     writeCommandAndNewline("A=M"); //A = SP
                     writeCommandAndNewline("D=M"); //D = M[SP], y
 
+                    //get SP, decrement and store in R0
+                    writeCommandAndNewline("@R0"); //A=0
+                    writeCommandAndNewline("M=M-1"); //M = SP - 1
+
                     //bit-wise and of x & y
-                    writeCommandAndNewline("A=A-1"); //Decrement SP
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("A=M");
                     writeCommandAndNewline("M=M&D"); //M = M[SP] & M[SP - 1}
+
+                    //increment SP
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("M=M+1");
                     break;
 
                 case "or":
@@ -176,9 +262,18 @@ public class CodeWriter {
                     writeCommandAndNewline("A=M"); //A = SP
                     writeCommandAndNewline("D=M"); //D = M[SP], y
 
+                    //get SP, decrement and store in R0
+                    writeCommandAndNewline("@R0"); //A=0
+                    writeCommandAndNewline("M=M-1"); //M = SP - 1
+
                     //bit-wise or of x | y
-                    writeCommandAndNewline("A=A-1"); //Decrement SP
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("A=M");
                     writeCommandAndNewline("M=M|D"); //M = M[SP] | M[SP - 1}
+
+                    //increment SP
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("M=M+1");
                     break;
 
                 case "not":
@@ -191,6 +286,10 @@ public class CodeWriter {
 
                     //bit-wise not of y
                     writeCommandAndNewline("M=!M"); //D = M[SP], y //TODO: not sure if this really works
+
+                    //increment SP
+                    writeCommandAndNewline("@R0");
+                    writeCommandAndNewline("M=M+1");
                     break;
                 default:
                     throw new RuntimeException("Arithmetic command " + command + " not recognized");
@@ -224,14 +323,20 @@ public class CodeWriter {
                 case "constant":
                     //the command "push constant value" should push the value onto the stack
                     try {
+                        //Load the constant into D reg
                         writeCommandAndNewline("@" + String.valueOf(index)); //load number in A reg
-                        writeCommandAndNewline("D=A");
+                        writeCommandAndNewline("D=A"); //D = number
+
+                        //Load the value of the SP into register A
                         writeCommandAndNewline("@R0");
-                        writeCommandAndNewline("A=M");
-                        writeCommandAndNewline("M=D");
-                        writeCommandAndNewline("D=A+1");
+                        writeCommandAndNewline("A=M"); //Set A = M[0]
+
+                        //Set M[SP] equal to the constant held in the D register
+                        writeCommandAndNewline("M=D"); //Set M[SP] = D = the constant we want to push on the stack
+
+                        //Increment the SP and save it at R0
                         writeCommandAndNewline("@R0");
-                        writeCommandAndNewline("M=D");
+                        writeCommandAndNewline("M=M+1");
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
