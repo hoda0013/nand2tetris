ff000de673fec338784eb189e20e787dc465e2f2
Dillon Hodapp <hoda0013@gmail.com>
2021-01-17 14:32:41 -0600

    refactor

--- Nand2TetrisPlayground/nand2tetris/src/main/java/com/example/chapter10/Main.kt
+++ Nand2TetrisPlayground/nand2tetris/src/main/java/com/example/chapter10/Main.kt
@@ -1,57 +1,8 @@
 package com.example.chapter10
 
-import java.util.ArrayList
-
 fun main(args: Array<String>) {
 
     val tokenizer = Tokenizer()
     tokenizer.initialize("/Users/bigern/Projects/nand2tetris/projects/10/Square/Main.jack")
-    tokenizer.tokenize2()
-//    tokenizer.tokenize()
-//    val tokens: MutableList<String> = ArrayList<String>().apply {
-//        add("class")
-//        add("Bar")
-//        add("{")
-//        add("field")
-//        add("int")
-//        add("var1")
-//        add(";")
-//        add("field")
-//        add("boolean")
-//        add("var2")
-//        add(";")
-//        add("static")
-//        add("FakeClassName")
-//        add("var3")
-//        add(",")
-//        add("var4")
-//        add(";")
-//        add("method")
-//        add("Fraction")
-//        add("foo")
-//        add("(")
-//        add("int")
-//        add("y")
-//        add(")")
-//        add("{")
-//        add("var")
-//        add("int")
-//        add("temp")
-//        add(";")
-//        add("let")
-//        add("temp")
-//        add("=")
-//        add("(")
-//        add("xxx")
-//        add("+")
-//        add("12")
-//        add(")")
-//        add("*")
-//        add("-")
-//        add("63")
-//    }
-//
-//    val parser = Parser()
-//    parser.setTokens(tokens)
-//    parser.parse()
+    tokenizer.tokenize()
 }
--- Nand2TetrisPlayground/nand2tetris/src/main/java/com/example/chapter10/Tokenizer.kt
+++ Nand2TetrisPlayground/nand2tetris/src/main/java/com/example/chapter10/Tokenizer.kt
@@ -1,13 +1,10 @@
 package com.example.chapter10
 
-import com.sun.org.apache.xpath.internal.operations.Bool
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileReader
 import java.io.FileWriter
-import java.lang.NumberFormatException
-import java.util.regex.Pattern
 
 
 class Tokenizer {
@@ -17,46 +14,22 @@ class Tokenizer {
     private lateinit var bufferedReader: BufferedReader
     private lateinit var inputFile: File
 
-    private val whiteSpaceRegex = Pattern.compile("[\\s]+")
+    private var tokenState: TokenState = TokenState.NORMAL
 
-    fun initialize(fileName: String){
+    fun initialize(fileName: String) {
         inputFile = File(fileName)
         outputFile = File(fileName.plus("T.xml"))
         bufferedWriter = BufferedWriter(FileWriter(outputFile))
         bufferedReader = BufferedReader(FileReader(inputFile))
     }
 
-    private val tokensSplitOnWhitespaceAndStrippedOfComments = ArrayList<String>()
-
-   enum class TokenState {
-       NORMAL,
-       COMMENT,
-       STRING_LITERAL;
-   }
-
-    private var tokenState: TokenState = TokenState.NORMAL
-    private var tokens = ArrayList<String>()
-
-    enum class TokenType(val value: String) {
-        KEYWORD("keyword"),
-        SYMBOL("symbol"),
-        INTEGER("integer"),
-        STRING("string"),
-        IDENTIFIER("identifier");
-    }
-
-    data class Token(val type: TokenType, val value: String) {
-        fun printTag(bufferedWriter: BufferedWriter) {
-            bufferedWriter.append("<${type.value}> $value </${type.value}>\n")
-        }
-    }
-
-    fun tokenize2() {
+    fun tokenize() {
         // Read line in
         var line = bufferedReader.readLine()
 
         bufferedWriter.append("<tokens>\n")
-        while(line != null) {
+
+        while (line != null) {
             if (tokenState == TokenState.COMMENT) {
                 // Does line contain the comment closing pattern?
                 if (line.contains("*/")) {
@@ -73,10 +46,9 @@ class Tokenizer {
                 if (line.contains("/*") || line.contains("/**")) {
                     if (line.contains("*/")) {
                         // This is a multi-line comment that is only using one line
-
                         line = bufferedReader.readLine()
                         continue
-                    }  else {
+                    } else {
                         // This is a multi line comment spread over multiple lines
                         tokenState = TokenState.COMMENT
 
@@ -85,7 +57,7 @@ class Tokenizer {
                     }
                 } else if (line.contains("//")) {
                     // This line contains a single line comment, strip out the comment part of the line the process like normal
-                   line = line.substringBefore("//")
+                    line = line.substringBefore("//")
                 }
 
                 // line has now been stripped of any comments
@@ -93,48 +65,15 @@ class Tokenizer {
                 var tempToken = ""
                 var isStringLiteral = false
 
+                // Iterate over each character in the line and split into tokens
                 line.toCharArray().forEach {
                     if (it.isWhitespace() && !isStringLiteral) {
-                        // tempToken can be processed if it's not empty
-                        if (tempToken.isNotEmpty()) {
-                           when {
-                               isKeyword(tempToken) -> {
-                                   Token(TokenType.KEYWORD, tempToken).printTag(bufferedWriter)
-                               }
-                               isSymbol(tempToken) -> {
-                                   Token(TokenType.SYMBOL, tempToken).printTag(bufferedWriter)
-                               }
-                               isIntConstant(tempToken) -> {
-                                   Token(TokenType.INTEGER, tempToken).printTag(bufferedWriter)
-                               }
-                               else -> {
-                                   Token(TokenType.IDENTIFIER, tempToken).printTag(bufferedWriter)
-                               }
-                           }
-
-                            tempToken = ""
-                        }
-
+                        printToken(tempToken)
+                        tempToken = ""
                     } else if (isSymbol(it.toString()) && !isStringLiteral) {
                         // a symbol can be next to an idnetifier or keyword so we need to process the existing token and the symbol is some cases
-                        if (tempToken.isNotEmpty()) {
-                            when {
-                                isKeyword(tempToken) -> {
-                                    Token(TokenType.KEYWORD, tempToken).printTag(bufferedWriter)
-                                }
-                                isSymbol(tempToken) -> {
-                                    Token(TokenType.SYMBOL, tempToken).printTag(bufferedWriter)
-                                }
-                                isIntConstant(tempToken) -> {
-                                    Token(TokenType.INTEGER, tempToken).printTag(bufferedWriter)
-                                }
-                                else -> {
-                                    Token(TokenType.IDENTIFIER, tempToken).printTag(bufferedWriter)
-                                }
-                            }
-
-                            tempToken = ""
-                        }
+                        printToken(tempToken)
+                        tempToken = ""
 
                         Token(TokenType.SYMBOL, it.toString()).printTag(bufferedWriter)
                     } else if (it == '"') {
@@ -160,6 +99,25 @@ class Tokenizer {
         bufferedWriter.close()
     }
 
+    private fun printToken(token: String) {
+        if (token.isNotEmpty()) {
+            when {
+                isKeyword(token) -> {
+                    Token(TokenType.KEYWORD, token).printTag(bufferedWriter)
+                }
+                isSymbol(token) -> {
+                    Token(TokenType.SYMBOL, token).printTag(bufferedWriter)
+                }
+                isIntConstant(token) -> {
+                    Token(TokenType.INTEGER, token).printTag(bufferedWriter)
+                }
+                else -> {
+                    Token(TokenType.IDENTIFIER, token).printTag(bufferedWriter)
+                }
+            }
+        }
+    }
+
     private fun isKeyword(value: String): Boolean {
         return Parser.Keyword.values().contains(value)
     }
@@ -177,145 +135,23 @@ class Tokenizer {
         }
     }
 
-
-    fun tokenize() {
-        // go through program line by line
-        // for each line, break into tokens based on whitespace
-        // categorize each token and print it to file
-       var r = bufferedReader.read()
-
-        var line = bufferedReader.readLine()
-
-        var isInMultiLineComment = false
-
-        while (line != null) {
-            var tokens = line.split(
-                    Regex("((?<=[;\\(\\)\\{\\}\\[\\]\\.\\,\\+\\-\\*\\/&\\|\\<\\>=~])|(?=[;\\(\\)\\{\\}\\[\\]\\.\\,\\+\\-\\*\\/&\\|\\<\\>=~]))")
-            )
-                    .mapNotNull { token ->
-                        if (token == "//" && !isInMultiLineComment) {
-                            // if we see this, and it's not contained in another multi-line comment  we can dispose of the rest of this line
-                            // break out of for loop, go to next line and continue processing
-                            null
-                        } else if (token == "/*" || token == "/**") {
-                            isInMultiLineComment = true
-                            null
-                            // discard this token and every other token until we see a */
-                        } else if (token == "*/") {
-                            // end comment
-                            isInMultiLineComment = false
-                            null
-                        } else if (!isInMultiLineComment) {
-                            if (token.isNotEmpty()) {
-                                token
-//                                tokensSplitOnWhitespaceAndStrippedOfComments.add(token)
-                            } else {
-                                null
-                            }
-                        } else {
-                            null
-                        }
-                    }
-            tokens
-        }
-
-//        var isInMultiLineComment = false
-//        while(line != null) {
-//        tokensSplitOnWhitespaceAndStrippedOfComments.addAll(
-//            line
-//                    .split(whiteSpaceRegex)
-//                    .mapNotNull { token ->
-//                        if (token == "//" && !isInMultiLineComment) {
-//                            // if we see this, and it's not contained in another multi-line comment  we can dispose of the rest of this line
-//                            // break out of for loop, go to next line and continue processing
-//                            null
-//                        } else if (token == "/*" || token == "/**") {
-//                            isInMultiLineComment = true
-//                            null
-//                            // discard this token and every other token until we see a */
-//                        } else if (token == "*/") {
-//                            // end comment
-//                            isInMultiLineComment = false
-//                            null
-//                        } else if (!isInMultiLineComment) {
-//                            if (token.isNotEmpty()) {
-//                                token
-////                                tokensSplitOnWhitespaceAndStrippedOfComments.add(token)
-//                            } else {
-//                                null
-//                            }
-//                        } else {
-//                            null
-//                        }
-//                    }
-//        )
-//            // Split at all whitespace
-//            val splitOnWhitespace = line.split(whiteSpaceRegex)
-//
-//            for(i in splitOnWhitespace.indices) {
-//                val token = splitOnWhitespace[i]
-//
-//                if (token == "//" && !isInMultiLineComment) {
-//                    // if we see this, and it's not contained in another multi-line comment  we can dispose of the rest of this line
-//                    // break out of for loop, go to next line and continue processing
-//                    break
-//                } else if (token == "/*" || token == "/**") {
-//                    isInMultiLineComment = true
-//                    // discard this token and every other token until we see a */
-//                } else if (token == "*/") {
-//                    // end comment
-//                    isInMultiLineComment = false
-//                } else if (!isInMultiLineComment) {
-//                    if (token.isNotEmpty()) {
-//                        tokensSplitOnWhitespaceAndStrippedOfComments.add(token)
-//                    }
-//                }
-//            }
-
-            line = bufferedReader.readLine()
-//        }
-
-//        var isInStringConstant = false
-//        var stringCollector = ""
-//        for (i in 0 until tokensSplitOnWhitespaceAndStrippedOfComments.size) {
-//
-//            val token = tokensSplitOnWhitespaceAndStrippedOfComments[i]
-//
-//            if (token == "\"" && isInStringConstant) {
-//                isInStringConstant = false
-//            } else if (token == "\"" && !isInStringConstant) {
-//                isInStringConstant = true
-//                // print string tag
-//            } else if (isInStringConstant) {
-//                stringCollector.plus("$token ")
-//            } else {
-//                stringCollector += token
-//
-//                // is token a keyword?
-//
-//                // is token a symbol?
-//
-//                // is token an integer?
-//
-//                // is token an identifier?
-//
-//            }
-//        }
-
-//        val tokens = ArrayList<String>()
-//
-//        tokensSplitOnWhitespaceAndStrippedOfComments.forEach {
-//            val splitted = it.split(
-//                    Regex("((?<=[;\\(\\)\\{\\}\\[\\]\\.\\,\\+\\-\\*\\/&\\|\\<\\>=~])|(?=[;\\(\\)\\{\\}\\[\\]\\.\\,\\+\\-\\*\\/&\\|\\<\\>=~]))")
-//            )
-//            splitted.filter { it.isNotEmpty() }.forEach { tokens.add(it) }
-//            System.out.println(splitted.toString())
-//        }
-//
-//        tokens.forEach {
-//            bufferedWriter.append("$it\n")
-//        }
-
-        bufferedWriter.close()
+    enum class TokenState {
+        NORMAL,
+        COMMENT;
     }
+
+    enum class TokenType(val value: String) {
+        KEYWORD("keyword"),
+        SYMBOL("symbol"),
+        INTEGER("integer"),
+        STRING("string"),
+        IDENTIFIER("identifier");
+    }
+
+    data class Token(val type: TokenType, val value: String) {
+        fun printTag(bufferedWriter: BufferedWriter) {
+            bufferedWriter.append("<${type.value}> $value </${type.value}>\n")
+        }
+    }
+
 }
