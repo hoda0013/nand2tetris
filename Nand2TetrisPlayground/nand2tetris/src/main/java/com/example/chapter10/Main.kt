dcde2a68d7834d24892cfab2cbc170c5ea013412
Dillon Hodapp <hoda0013@gmail.com>
2021-01-17 14:51:36 -0600

    saving

--- Nand2TetrisPlayground/nand2tetris/src/main/java/com/example/chapter10/Main.kt
+++ Nand2TetrisPlayground/nand2tetris/src/main/java/com/example/chapter10/Main.kt
@@ -1,8 +1,25 @@
 package com.example.chapter10
 
-fun main(args: Array<String>) {
+import java.io.File
 
+fun main(args: Array<String>) {
     val tokenizer = Tokenizer()
-    tokenizer.initialize("/Users/bigern/Projects/nand2tetris/projects/10/Square/Main.jack")
-    tokenizer.tokenize()
+
+    val fileOrDirectory = "/Users/bigern/Projects/nand2tetris/projects/10/Square/"
+    val file = File(fileOrDirectory)
+
+    if (file.exists()) {
+        if (file.isFile) {
+            tokenizer.initialize(fileOrDirectory)
+            tokenizer.tokenize()
+        } else if (file.isDirectory) {
+            // get all .jack files in directory and process them
+            file.list()
+                    ?.filter { it.endsWith(".jack") }
+                    ?.forEach {
+                        tokenizer.initialize("$fileOrDirectory$it")
+                        tokenizer.tokenize()
+                    }
+        }
+    }
 }
